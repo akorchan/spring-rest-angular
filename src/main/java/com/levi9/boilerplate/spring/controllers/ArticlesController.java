04ccaebc69f82ef59adac7197fdb07534a9f893c
Andrey Korchan <gprogramador@gmail.com>
2016-03-13 14:11:10 +0200

    switch to MySQL

--- src/main/java/com/levi9/boilerplate/spring/controllers/ArticlesController.java
+++ src/main/java/com/levi9/boilerplate/spring/controllers/ArticlesController.java
@@ -3,6 +3,7 @@ package com.levi9.boilerplate.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
